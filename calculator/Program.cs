cc45b524eb1ccd39ad30b7e43e976c518033deb9
Antiukova <Antiukova@lab4.com>
2022-11-22 21:02:42 +0200

    jusr pattern

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("CALCULATOR \nSelect mode: \n1 - ordinary \n2 - advanced \nEnter 0 to exit.");
+            string input = Console.ReadLine();
+            while (input != "exit")
+            {
+                if (input == "1")
+                {
+                    string operation = Console.ReadLine();
+
+                }
+                else if (input == "2")
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine("Please, choose number.");
+                }
+            }
+        }
+    }
+}
