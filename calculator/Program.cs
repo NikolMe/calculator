df128b5e1ccda420106f3558704029ff61bcae21
ShcherbaNataliia <118847235+ShcherbaNataliia@users.noreply.github.com>
2022-11-29 22:31:05 +0200

    Update Program.cs

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -8,6 +8,11 @@ namespace calculator
 {
     internal class UsualCalculator
     {
+        internal UsualCalculator()
+        {
+            Console.Write("You are using a usual calculator");
+        }
+        
         public double num1;
         public double num2;
 
@@ -28,20 +33,38 @@ namespace calculator
 
         protected internal double Division()
         {
-            return (num1 / num2);
+            if (num2 == 0)
+            { 
+              Console.WriteLine("Incorrect value! It should not be 0.");
+              return -1; 
+            }
+            else return (num1 / num2);
         }
     }
    
     internal class EngineerCalculator : UsualCalculator
     {
-        protected internal static double Log(double num)
+        public double num;
+        
+        internal EngineerCalculator()
+        {
+            Console.WriteLine("and an engineer calculator");
+        }
+        
+        protected internal double Log()
         {
             return Math.Log(num);
         }
 
-        protected internal static double Sqrt(double num)
+        protected internal double Sqrt()
         {
-            return Math.Sqrt(num);
+           if (num < 0)
+            {
+                Console.WriteLine("Incorrect value! It should be positiv");
+                return -1;
+            }
+            
+            else return Math.Sqrt(num);
         }
     }
     
